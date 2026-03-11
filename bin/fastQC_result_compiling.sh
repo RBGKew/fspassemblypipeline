8a06231e92c8a4edcfd11fe4f7a17f80db5750b5
Wu <hazelhuang1993@icloud.com>
2026-03-11 16:47:24 +0000

    pass all falco output files to the bash script and grep different content by the different file extension.

--- bin/fastQC_result_compiling.sh
+++ bin/fastQC_result_compiling.sh
@@ -1,15 +1,57 @@
-summary_files=("$@")
+shopt -s nullglob
 
-for i in */; do grep 'Filename' "$i"/*fastqc_data.txt; done | awk '{split($2,a,".");print a[1]}' > name.txt
-for i in */; do grep '%GC' "$i"/*fastqc_data.txt; done | awk '{print $2}' > GC.txt
-for i in */; do grep 'Total Sequences' "$i"/*fastqc_data.txt; done | awk '{print $3}' > total_reads_no.txt
-for i in */; do grep 'Total Deduplicated Percentage' "$i"/*fastqc_data.txt; done | awk '{print 100-$4}' > duplication_level.txt
+input_files=("$@")
+fastqc_data_files=()
+summary_files=()
 
-if [[ ${#summary_files[@]} -gt 0 ]]; then
-	grep -h 'Per sequence GC content' "${summary_files[@]}" | awk '{print $1}' > GC_pass.txt
-else
-	for i in */; do grep -h 'Per sequence GC content' "$i"/*gz_summary.txt; done | awk '{print $1}' > GC_pass.txt
+for f in "${input_files[@]}"; do
+	[[ -e "$f" ]] || continue
+	if [[ "$f" == *_fastqc_data.txt ]]; then
+		fastqc_data_files+=("$f")
+	elif [[ "$f" == *_summary.txt || "$f" == *gz_summary.txt ]]; then
+		summary_files+=("$f")
+	fi
+done
+
+if [[ ${#fastqc_data_files[@]} -eq 0 ]]; then
+	for i in */; do
+		[[ -d "$i" ]] || continue
+		for f in "$i"/*fastqc_data.txt; do
+			[[ -e "$f" ]] || continue
+			fastqc_data_files+=("$f")
+		done
+	done
 fi
+
+if [[ ${#fastqc_data_files[@]} -eq 0 ]]; then
+	echo "ERROR: No *fastqc_data.txt files found." >&2
+	exit 1
+fi
+
+if [[ ${#summary_files[@]} -eq 0 ]]; then
+	summary_files=( *_summary.txt )
+fi
+
+if [[ ${#summary_files[@]} -eq 0 ]]; then
+	for i in */; do
+		[[ -d "$i" ]] || continue
+		for f in "$i"/*_summary.txt; do
+			[[ -e "$f" ]] || continue
+			summary_files+=("$f")
+		done
+	done
+fi
+
+if [[ ${#summary_files[@]} -eq 0 ]]; then
+	echo "ERROR: No summary files found." >&2
+	exit 1
+fi
+
+grep -h 'Filename' "${fastqc_data_files[@]}" | awk '{split($2,a,".");print a[1]}' > name.txt
+grep -h '%GC' "${fastqc_data_files[@]}" | awk '{print $2}' > GC.txt
+grep -h 'Total Sequences' "${fastqc_data_files[@]}" | awk '{print $3}' > total_reads_no.txt
+grep -h 'Total Deduplicated Percentage' "${fastqc_data_files[@]}" | awk '{print 100-$4}' > duplication_level.txt
+grep -h 'Per sequence GC content' "${summary_files[@]}" | awk '{print $1}' > GC_pass.txt
 
 echo "name	GC	GC_pass	total_reads_no	Duplicated Percentage" > fastQC_result.txt
 paste name.txt GC.txt GC_pass.txt total_reads_no.txt duplication_level.txt >> fastQC_result.txt
